da4f05da560d127f1760e4d609cd5005740fcd82
Dan Toškan <dan.toskan3@gmail.com>
2024-10-29 12:59:46 +0100

    Attempt to add log handler support #3

--- libcef_dll/base/cef_logging.cc
+++ libcef_dll/base/cef_logging.cc
@@ -156,16 +156,6 @@ template std::string* MakeCheckOpString<std::string, std::string>(
     const char* name);
 #endif
 
-void SetLogMessageHandler(LogMessageHandlerFunction handler)
-{
-  ::logging::SetLogMessageHandler(handler);
-}
-
-LogMessageHandlerFunction GetLogMessageHandler()
-{
-  return ::logging::GetLogMessageHandler();
-}
-
 #if defined(OS_WIN)
 LogMessage::SaveLastError::SaveLastError() : last_error_(::GetLastError()) {}
 
