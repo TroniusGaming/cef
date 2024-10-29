07ab6f6b1f621897525e9e36fcf4d6f85da7d1ca
Dan Toškan <dan.toskan3@gmail.com>
2024-10-29 12:58:33 +0100

    Attempt to add log handler support #2

--- include/base/cef_logging.h
+++ include/base/cef_logging.h
@@ -205,10 +205,11 @@ int GetVlogLevel(const char (&file)[N]) {
 // it's sent to other log destinations (if any).
 // Returns true to signal that it handled the message and the message
 // should not be sent to other log destinations.
-typedef bool (*LogMessageHandlerFunction)(int severity,
-    const char* file, int line, size_t message_start, const std::string& str);
-CEF_EXPORT void SetLogMessageHandler(LogMessageHandlerFunction handler);
-CEF_EXPORT LogMessageHandlerFunction GetLogMessageHandler();
+typedef int (*LogMessageHandlerFunction)(int severity,
+    const char* file, int line, size_t message_start, const char* str);
+inline void SetLogMessageHandler(LogMessageHandlerFunction handler) {
+  cef_set_log_handler(handler);
+}
 
 typedef int LogSeverity;
 const LogSeverity LOG_VERBOSE = -1;  // This is level 1 verbosity
