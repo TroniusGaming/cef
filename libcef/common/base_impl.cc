86dafdfa63e0e52aa6e3efa847770fdde027bde8
Dan Toškan <dan.toskan3@gmail.com>
2024-10-29 14:35:47 +0100

    Attempt to add log handler support #6

--- libcef/common/base_impl.cc
+++ libcef/common/base_impl.cc
@@ -218,9 +218,10 @@ CEF_EXPORT void cef_log(const char* file,
 }
 
 CEF_EXPORT void cef_set_log_handler(LogMessageHandlerFunction handler) {
-  logging::SetLogMessageHandler([handler](int severity,
+  static LogMessageHandlerFunction gCefLogHandler = handler;
+  logging::SetLogMessageHandler([](int severity,
     const char* file, int line, size_t message_start, const std::string& str) -> bool {
-      return handler(severity, file, line, message_start, str.c_str());
+      return gCefLogHandler(severity, file, line, message_start, str.c_str());
   });
 }
 
